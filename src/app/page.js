a35835ce9d4872c1dbd1afb38e685f5aeb189d78
ahmed barkhed <davidmohmed@gmail.com>
2025-08-18 20:15:06 +0400

    eeee

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 import Header from '../components/Header';
-import Hero from '../components/Hero';
+import Hero from '../components/hero';
 import TechnologySections from '../components/TechnologySections';
 import FeaturesSection from '../components/FeaturesSection';
 import Footer from '../components/Footer';
