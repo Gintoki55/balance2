73996e6d03d8a0774f6cffce3d1f1281698c9f03
ahmed barkhed <davidmohmed@gmail.com>
2025-09-10 17:39:16 +0400

    N

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,13 +1,21 @@
-"use client";
+// "use client";
+import StationHeader from "@/components/stationheader";
 import React from "react";
-import { ArrowLeft } from "lucide-react";
-// import { medData, msfData, rogData } from "../../components/designs"; // استدعاء البيانات
 
-function DeviceTable({ device }) {
+export default function Dashboard() {
   return (
-    <div className="bg-white rounded-xl shadow p-4 mb-6">
-      
+    <div className="bg-[#F9FAFB] min-h-screen flex flex-col">
+      {/* الهيدر */}
+      <StationHeader title="Dashboard"/>
+
+      {/* محتوى الصفحة المؤقت */}
+      <main className="flex-grow flex items-center justify-center">
+        <h2 className="text-2xl sm:text-3xl font-semibold text-gray-700">
+          This will be the Dashboard
+        </h2>
+      </main>
     </div>
   );
 }
+
 
